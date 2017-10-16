248afe65dcd6db1a001892384b6a65a2ee454d4c
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-10-16 14:59:56 +0200

    minor: apply small changes (_index.js, module.js)

--- src/_index.js
+++ src/_index.js
@@ -1,120 +1,103 @@
 /* eslint quotes: [0], strict: [0] */
+
 const {
-    _,
-    $d,
-    $o,
-    $fs,
-    $b,
-    $s
-    // $r.stdin() -> Promise  ;; to read from stdin
-} = require('zaccaria-cli');
+  _,
+  $d,
+  $o,
+  $fs,
+  $b,
+  $s
+  // $r.stdin() -> Promise  ;; to read from stdin
+} = require("zaccaria-cli");
 
-const path = require('path');
+const path = require("path");
 
-const debug = require('debug');
+const debug = require("debug");
 const shelljs = $s;
 const bluebird = $b;
 
-let {
-    error,
-    ok,
-    info,
-    warn
-} = require('./messages')
+let { error, ok, info, warn } = require("./messages");
 
 let readLocal = f => {
-    const curdir = path.dirname($fs.realpathSync(__filename));
-    const filepath = path.join(curdir, `../${f}`);
-    return $fs.readFileAsync(filepath, 'utf8');
-}
-
+  const curdir = path.dirname($fs.realpathSync(__filename));
+  const filepath = path.join(curdir, `../${f}`);
+  return $fs.readFileAsync(filepath, "utf8");
+};
 
 const getOptions = doc => {
-    "use strict";
-    const o = $d(doc);
-    const help = $o('-h', '--help', false, o);
-    const all = o.all;
-    const justone = o.justone;
-    const dir = o.DIR;
-    const endpoint = o.ENDPOINT;
-    const num = o.NUM;
-    return {
-        help,
-        dir,
-        endpoint,
-        all,
-        justone,
-        num
-    };
-}
+  "use strict";
+  const o = $d(doc);
+  const help = $o("-h", "--help", false, o);
+  const all = o.all;
+  const justone = o.justone;
+  const dir = o.DIR;
+  const endpoint = o.ENDPOINT;
+  const num = o.NUM;
+  return {
+    help,
+    dir,
+    endpoint,
+    all,
+    justone,
+    num
+  };
+};
 
-let agent = require('superagent-promise')(require('superagent'), bluebird);
+let agent = require("superagent-promise")(require("superagent"), bluebird);
 
-let _module = require('./module');
+let _module = require("./module");
 _module = _module({
-    debug,
-    _,
-    shelljs,
-    bluebird,
-    agent
-})
+  debug,
+  _,
+  shelljs,
+  bluebird,
+  agent
+});
 
-let {
-    execAsync,
-    extractExercises,
-    testSolution,
-    packet
-} = _module;
+let { execAsync, extractExercises, testSolution, packet } = _module;
 
-let _gl = (s) => {
-    return s.split("\n")[0];
-}
+let _gl = s => {
+  return s.split("\n")[0];
+};
 
 function testIt(endpoint) {
-    return function(it,num) {
-        return testSolution(`http://${endpoint}/payload`, it).then((r) => {
-            if (r.body.correct) {
-                ok(`${num}: ${_gl(it.code.solution)}`);
-            } else {
-                error(`${num}: ${it.code.solution} ==> ${it.code.validation}`);
-            }
-        });
-    }
+  return function(it, num) {
+    return testSolution(`http://${endpoint}/payload`, it).then(r => {
+      if (r.body.correct) {
+        ok(`${num}: ${_gl(it.code.solution)}`);
+      } else {
+        error(`${num}: ${it.code.solution} ==> ${it.code.validation}`);
+      }
+    });
+  };
 }
 
 const main = () => {
-    readLocal('docs/usage.md').then(it => {
-        const {
-            help,
-            dir,
-            endpoint,
-            all,
-            justone,
-            num
-        } = getOptions(it);
-        if (help) {
-            console.log(it);
-        } else {
-            execAsync('npm bin').then((bin) => {
-                bin = bin.stdout.replace(/(\r\n|\n|\r)/gm,"");
-                execAsync(`SILENT=1 ${bin}/gitbook3toedx json ${dir}`)
-                    .then(({
-                        stdout
-                    }) => JSON.parse(stdout))
-                    .then(extractExercises)
-                    .then((xs) => {
-                        if (justone) {
-                            testIt(endpoint)(xs[num],num)
-                        } else if (all) {
-                            bluebird.all(_.map(xs, testIt(endpoint)));
-                        }
-                    });
-            });
-        }
-    });
-}
+  readLocal("docs/usage.md").then(it => {
+    const { help, dir, endpoint, all, justone, num } = getOptions(it);
+    if (help) {
+      console.log(it);
+    } else {
+      execAsync("npm bin").then(bin => {
+        bin = bin.stdout.replace(/(\r\n|\n|\r)/gm, "");
+        execAsync(`SILENT=1 ${bin}/gitbook3toedx json ${dir}`)
+          .then(({ stdout }) => JSON.parse(stdout))
+          .then(extractExercises)
+          .then(xs => {
+            if (justone) {
+              testIt(endpoint)(xs[num], num);
+            } else if (all) {
+              bluebird.all(_.map(xs, testIt(endpoint)));
+            }
+          });
+      });
+    }
+  });
+};
 
-
-module.exports = _.assign({
+module.exports = _.assign(
+  {
     main
-}, _module);
+  },
+  _module
+);
--- src/module.js
+++ src/module.js
@@ -1,58 +1,54 @@
-let _module = ({
-    _, shelljs, bluebird, agent
-}) => {
-    if (_.any([_, bluebird, shelljs, agent ], _.isUndefined)) {
-        throw "Missing dependency"
-    }
+let _module = ({ _, shelljs, bluebird, agent }) => {
+  if (_.any([_, bluebird, shelljs, agent], _.isUndefined)) {
+    throw "Missing dependency";
+  }
 
-    let execAsync = (cmd) => {
-        return new bluebird((resolve) => {
-            shelljs.exec(cmd, {async: true, silent: true}, (code, stdout) => {
-                resolve({code, stdout});
-            });
-        });
-    };
+  let execAsync = cmd => {
+    return new bluebird(resolve => {
+      shelljs.exec(cmd, { async: true, silent: true }, (code, stdout) => {
+        resolve({ code, stdout });
+      });
+    });
+  };
 
-    let extractExercises = (json) => {
-        json = _.flatten(_.map(json.chapters, (c) => c.sequentials));
-        json = _.flatten(_.map(json, (c) => c.verticals));
-        json = _.filter(json, (v) => v.type === 'exercise');
-        return json;
-    };
+  let extractExercises = json => {
+    json = _.flatten(_.map(json.chapters, c => c.sequentials));
+    json = _.flatten(_.map(json, c => c.verticals));
+    json = _.filter(json, v => v.type === "exercise");
+    return json;
+  };
 
-    let $ = (it) => JSON.stringify(it, 0, 4);
+  let $ = it => JSON.stringify(it, 0, 4);
 
-    let packet = (exe) => {
-        return {
-            xqueue_body: $({
-
-                student_info: $({
-                    anonimized_id: 3281
-                }),
-
-                student_response: exe.code.solution,
-                grader_payload: exe.grader_payload
-            })
-        };
-    };
-
-    let testSolution = (endpoint, exercise) => {
-        return agent
-            .post(endpoint)
-            .set('Accept', 'application/json')
-            .send(packet(exercise))
-            .end();
-
-    };
-
+  let packet = exe => {
     return {
-        readHelp: () => {
-            return execAsync('cat ../docs/usage.md');
-        },
-        execAsync: execAsync,
-        extractExercises: extractExercises,
-        testSolution: testSolution,
-        packet: packet
+      xqueue_body: $({
+        student_info: $({
+          anonimized_id: 3281
+        }),
+
+        student_response: exe.code.solution,
+        grader_payload: exe.grader_payload
+      })
     };
-}
+  };
+
+  let testSolution = (endpoint, exercise) => {
+    return agent
+      .post(endpoint)
+      .set("Accept", "application/json")
+      .send(packet(exercise))
+      .end();
+  };
+
+  return {
+    readHelp: () => {
+      return execAsync("cat ../docs/usage.md");
+    },
+    execAsync: execAsync,
+    extractExercises: extractExercises,
+    testSolution: testSolution,
+    packet: packet
+  };
+};
 module.exports = _module;
